20fc55187663edbf7cdb9dcd9c167e46e7d07f5f
Ilia Vasilev <vasilya93@gmail.com>
2017-07-05 18:24:29 +0200

    implementer tracking of waypoints

--- scripts/pid_controller.py
+++ scripts/pid_controller.py
@@ -4,32 +4,23 @@ class pd_controller:
         self._p_coef = p_coef
         self._d_coef = d_coef
         self._limit_out = limit_out
-        self._value_desired = 0.0
-	self._input_current = 0.0
-	self._error_previous = 0.0
 
-    def set_value_desired(self, value_desired):
-        desired_diff = value_desired - self._value_desired
-        self._value_desired = value_desired
-        error_current = self._value_desired - self._input_current
-        output = self._p_coef * error_current + self._d_coef * desired_diff
+        self._previous_error = 0.0
+        self._is_error_initialized = False
+
+    def set_current_error(self, error):
+        output = error * self._p_coef
+
+        if self._is_error_initialized:
+            error_diff = error - self._previous_error
+            output += self._d_coef * error_diff
+            self._previous_error = error
+        else:
+            self._previous_error = error
+            self._is_error_initialized = True
+
 	if output > self._limit_out:
             output = self._limit_out
         elif output < (-self._limit_out):
             output = (-self._limit_out)
         return output
-
-    def set_value_current(self, value_current):
-        value_diff = self._input_current - value_current 
-        self._input_current = value_current
-        error_current = self._value_desired - self._input_current
-        output = self._p_coef * error_current + self._d_coef * value_diff
-	if output > self._limit_out:
-            output = self._limit_out
-        elif output < (-self._limit_out):
-            output = (-self._limit_out)
-        return output
-
-    def get_error(self):
-        return self._input_current - self._value_desired
-
--- scripts/route_manager.py
+++ scripts/route_manager.py
@@ -1,6 +1,11 @@
+from geometry_msgs.msg import Point
+from copy import copy
 import numpy as np
 
 ERROR_THRESHOLD = 0.1
+TIME_POINT_REACHED = 1.0
+
+# what is the usage scenario of the class?
 
 class RouteManager:
     def __init__(self):
@@ -8,9 +13,41 @@ class RouteManager:
             Point(-2.0, 0.5, 0.9), \
             Point(-2.0, 2.0, 0.9), \
             Point(-0.5, 2.0, 0.9), \
-            Point(-0.5, 0.5, 0.9)
+            Point(-0.5, 0.5, 0.9)]
         self._current_waypoint = -1
+        self._is_route_finished = False
         self._time_big_error = 0.0
+        self._current_position = Point(0.0, 0.0, 0.0)
+        self._current_error = Point(0.0, 0.0, 0.0)
+
+    def set_current_position(self, current_position, time_current):
+        self._current_position = copy(current_position)
+
+        if self._current_waypoint == -1:
+            if len(self._waypoints) > 0:
+                self._current_waypoint = 1
+                print("Info: copter went for waypoint number %d" % self._current_waypoint)
+            else:
+                print("Warning: there are no waypoints in RouteManager!")
+                return
+
+        self._calculate_current_error()
+
+        if np.abs(self._current_error.x) < ERROR_THRESHOLD and \
+                np.abs(self._current_error.y) < ERROR_THRESHOLD and \
+                np.abs(self._current_error.z) < ERROR_THRESHOLD:
+            time_difference = time_current - self._time_big_error
+            if time_difference > TIME_POINT_REACHED:
+                print("waypoint was reached")
+                self._time_big_error = time_current
+                self._current_waypoint += 1
+                print("Info: copter went for waypoint number %d" % self._current_waypoint)
+                if self._current_waypoint >= len(self._waypoints):
+                    self._current_waypoint = -1
+                    self._is_route_finished = True
+                self._calculate_current_error()
+        else:
+            self._time_big_error = time_current
 
     def get_current_waypoint(self):
         if self._current_waypoint == -1:
@@ -18,40 +55,18 @@ class RouteManager:
         else:
             return self._waypoints[self._current_waypoint]
 
-    def is_waypoint_reached(self, position, time_current):
-        if self._current_waypoint == -1:
-            if len(self._waypoints) > 0:
-                self._current_waypoint = 1
-            else:
-                print("Warning: there are no waypoints in RouteManager!")
-                return True
-
-        is_waypoint_reached = False
-        x_error, y_error, z_error = self._calculate_errors(position)
-
-        if np.abs(x_error) < ERROR_THRESHOLD and \
-                np.abs(y_error) < ERROR_THRESHOLD and \
-                np.abs(z_error) < ERROR_THRESHOLD:
-            time_difference = time_current - self._time_big_error
-            if time_difference > TIME_POINT_REACHED:
-                is_waypoint_reached = True
-                print("waypoint was reached")
-
-                self._current_waypoint += 1
-                if self._current_waypoint >= len(self._waypoints):
-                    self._current_waypoint = -1
-        else:
-            self._time_big_error = rospy.get_time()
-            is_waypoint_reached = False
-
-        return is_waypoint_reached
+    def get_current_error(self):
+        return self._current_error
 
     def is_route_finished(self):
-        return True if self._current_waypoint == -1 else False
+        return self._is_route_finished
 
-    def _calculate_errors(self, position):
-        x_error = position.x - self._waypoints[self._current_waypoint].x
-        y_error = position.y - self._waypoints[self._current_waypoint].y
-        z_error = position.z - self._waypoints[self._current_waypoint].z
-        return (x_error, y_error, z_error)
+    def _calculate_current_error(self):
+        if self._current_waypoint == -1:
+            self._current_error = Point(0.0, 0.0, 0.0)
+            return
 
+        x_error = self._current_position.x - self._waypoints[self._current_waypoint].x
+        y_error = self._current_position.y - self._waypoints[self._current_waypoint].y
+        z_error = self._current_position.z - self._waypoints[self._current_waypoint].z
+        self._current_error = Point(x_error, y_error, z_error)
